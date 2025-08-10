2de1f8c96c846767725621fb994cd5d6c619b1fa
SudhanshuDeshmukh2003 <sudhanshudeshmukh566@gmail.com>
2025-08-11 00:59:29 +0530

    Updated project

---
+++ Wheels_on_Demand/Backend/CarMarketAPI/CarMarketAPI/Services/UserService.cs
@@ -0,0 +1,46 @@
+﻿using CarMarketAPI.Data;
+using CarMarketAPI.DTOs;
+
+
+namespace CarMarketAPI.Services
+{
+    public class UserService 
+    { 
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserDTO> GetByIdAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null) return null;
+
+            return new UserDTO
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Phone = user.Phone
+            };
+        }
+
+        public async Task<UserDTO> UpdateAsync(int id, UserDTO dto)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null) return null;
+
+            user.FirstName = dto.FirstName;
+            user.LastName = dto.LastName;
+            user.Email = dto.Email;
+            user.Phone = dto.Phone;
+
+            await _context.SaveChangesAsync();
+
+            return dto;
+        }
+    }
+}
